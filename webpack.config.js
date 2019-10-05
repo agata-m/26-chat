6a213a0ac672eccbc37686a88cd694e7565ea2c9
Agata Musiuk <agata.musiuk@gmail.com>
2019-10-05 18:55:11 +0200

    Add server index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 
 
-const plugins = [
-    new HtmlWebpackPlugin ({
-        template: 'client/index.html',
-        filename: 'index.html',
-        inject: 'body'
-    })
-];
-
 module.exports = (env) => {
     const environment = env || 'production';
-    
+
+    const plugins = [
+        new HtmlWebpackPlugin ({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ];
+
     if (env === 'production') {
         plugins.push(
             new OptimizeJsPlugin({
