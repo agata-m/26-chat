82e7b0dc4c9d5c11c65bde65ba664beb3363e2d1
Agata Musiuk <agata.musiuk@gmail.com>
2019-10-05 11:19:38 +0200

    Update JS and styles

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+
+const plugins = [
+    new HtmlWebpackPlugin ({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })
+];
+
+module.exports = (env) => {
+    const environment = env || 'production';
+    
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
+    }
+
+    return {
+        mode: environment,
+        entry: './client/index.js',
+        plugins,
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'app.bundle.js'
+        },
+        
+        devServer: {
+            proxy: {
+                '/socket.io': {
+                    target: 'http://localhost:3000',
+                    ws: true
+                }
+            }
+        }
+    }
+};
